3751b8275ded80b76ff67548d6bffedb7946ed11
Dan Gilleland <dgilleland@nait.ca>
2021-05-26 13:15:56 -0600

    Topic C - Person Modifications

--- src/ABCs/Topic/C/Person.cs
+++ src/ABCs/Topic/C/Person.cs
@@ -21,5 +21,12 @@ namespace Topic.C
             get { return _Age; }
             set { _Age = value; }
         }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
     }
 }
