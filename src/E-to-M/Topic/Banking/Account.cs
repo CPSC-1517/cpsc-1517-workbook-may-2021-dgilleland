a8cea9ad7d65d2e7440c74f73810c82a100726f2
Dan Gilleland <dgilleland@nait.ca>
2021-05-26 16:35:43 -0600

    Topic G

--- src/E-to-M/Topic/Banking/Account.cs
+++ src/E-to-M/Topic/Banking/Account.cs
@@ -15,13 +15,23 @@ namespace Topic.Banking
         public double Balance
         {
             get { return _Balance; }
-            private set { _Balance = value; }
+            private set
+            {
+                if (value < -OverdraftLimit)
+                    throw new Exception("Negative balances cannot exceed the Overdraft Limit");
+                _Balance = value;
+            }
         }
 
         public double OverdraftLimit
         {
             get { return _OverdraftLimit; }
-            set { _OverdraftLimit = value; }
+            set
+            {
+                if (value < 0)
+                    throw new Exception("Negative overdraft limits not allowed");
+                _OverdraftLimit = value;
+            }
         }
 
         public string AccountType { get; }
@@ -46,6 +56,22 @@ namespace Topic.Banking
         #region Constructor
         public Account(string bankName, int branchNumber, int institutionNumber, int accountNumber, double balance, double overdraftLimit, string accountType)
         {
+            // Ensuring a valid state
+            if (string.IsNullOrEmpty(bankName) || string.IsNullOrEmpty(bankName.Trim()))
+                throw new Exception("BankName is required");
+            if (branchNumber < 10000 || branchNumber > 99999)
+                throw new Exception("Branch number must be 5 digits");
+            if (institutionNumber < 100 || institutionNumber > 999)
+                throw new Exception("InstitutionNumber must be a three-digit value");
+            if (balance <= 0)
+                throw new Exception("Opening balance must be greater than zero");
+            if (balance != Math.Round(balance, 2))
+                throw new Exception("Opening balances cannot include a fraction of a penny");
+            if (overdraftLimit != Math.Round(overdraftLimit, 2))
+                throw new Exception("Overdraft limit amounts cannot include a fraction of a penny");
+            if (string.IsNullOrEmpty(accountType) || string.IsNullOrEmpty(accountType.Trim()))
+                throw new Exception("Account type cannot be empty");
+
             BankName = bankName;
             BranchNumber = branchNumber;
             InstitutionNumber = institutionNumber;
@@ -59,6 +85,11 @@ namespace Topic.Banking
         #region Methods
         public double Withdraw(double amount)
         {
+            if (amount != Math.Round(amount, 2))
+                throw new Exception("Withdrawal amounts cannot include fractions of a penny");
+            if (amount > Balance + OverdraftLimit)
+                throw new Exception("Insufficient Funds");
+
             if (amount <= Balance + OverdraftLimit)
                 Balance = Balance - amount;
             else
@@ -68,7 +99,9 @@ namespace Topic.Banking
 
         public void Deposit(double amount)
         {
-            Balance += amount;
+            if (amount != Math.Round(amount, 2))
+                throw new Exception("Deposit amounts cannot include fractions of a penny");
+            Balance = Math.Round(Balance + amount, 2);
         }
         #endregion
     }
