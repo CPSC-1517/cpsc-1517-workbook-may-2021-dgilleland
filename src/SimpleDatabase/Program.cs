695dd8c0b5cacb227673c99fd005eb891d35bd7b
Dan Gilleland <dgilleland@nait.ca>
2021-06-14 18:50:07 -0600

    Simple check to read connection string
    Not working for some reason....

--- src/SimpleDatabase/Program.cs
+++ src/SimpleDatabase/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace SimpleDatabase
 {
@@ -6,7 +9,25 @@ namespace SimpleDatabase
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Database!");
+            var app = new Program();
+            app.Run();
+        }
+
+        private readonly IConfiguration _configuration; // this will allow access to appsettings
+
+        public Program()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json", true, true);
+            _configuration = builder.Build();
+        }
+
+        private void Run()
+        {
+            // Just to show that it works....
+            string conn = _configuration.GetConnectionString("DefaultConnection");
+            Console.WriteLine($"'{conn}'");
         }
     }
 }
