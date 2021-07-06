04865e70a9f80e6f3940d863eb89f527e4fca663
Dan Gilleland <dgilleland@nait.ca>
2021-07-05 19:13:47 -0600

    File Upload Demo

--- src/RazorWebApp/Pages/Index.cshtml.cs
+++ src/RazorWebApp/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -26,6 +27,35 @@ namespace RazorWebApp.Pages
         public string ContentRootPath
         { get { return _environment.ContentRootPath; } }
 
+        [BindProperty]
+        public IFormFile UserFile { get; set; }
+
+        public void OnPost()
+        {
+            // if there is a UserFile (not null)...
+            if (UserFile != null)
+            {
+                //      if I do not have an "Uploads" folder in my ContentRootPath
+                string uploadsFolder = System.IO.Path.Combine(ContentRootPath, "Uploads");
+                if(!System.IO.Directory.Exists(uploadsFolder))
+                    //          then create that folder
+                    System.IO.Directory.CreateDirectory(uploadsFolder);
+                string filePath = System.IO.Path.Combine(uploadsFolder, UserFile.FileName); // not too secure
+                if(!System.IO.File.Exists(filePath))
+                //      if I do not have the user file already
+                {
+                    //          then save that file
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        UserFile.CopyTo(stream);
+                    }
+                }
+                else
+                {
+                //          else set a message saying that I already have that file
+                }
+            }
+        }
 
         public void OnGet()
         {
